3aa0e7bc7bec44ee6d06da87ad8625115d00e61f
Liza_Bekker <116813561+LizaBekker@users.noreply.github.com>
2022-12-14 20:02:24 +0300

    Отправляю солянку по треугольнику, как я ее вижу.

---
+++ Trinity.java
@@ -0,0 +1,38 @@
+public class Trinity {
+    {
+        public static void main(String[]Object args;
+        args)
+        {
+            Point A; //вершина А
+            Point B; //вершина В
+            Point C; //вершина С
+
+            A = new Point(double x1, double y1); //координаты А
+            B = new Point(double x2, double y2); //координаты В
+            C = new Point(double x3, double y3); //координаты С
+        }
+
+
+
+        public int lenAB,lenBC, lenAC; //ввести переменные длины
+        {
+            double x1,x2,x3; //программа предлоджила ввести double, иначе коориднаты горят красным
+            double y1,y2,y3;
+
+            return lenAB = sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1))); //формула отрезка АВ через квадрат и ее возвращение
+            return lenBC = sqrt(((x3-x1)*(x3-x2))+(y3-y2)*(y3-y2)));
+            return lenAC = sqrt(((x3-x1)*(x3-x1))+(y3-y1)*(y3-y1)));
+        }
+        public int PerimetrABC; //переменная периметра
+        {
+            return PerimetrABC = (lenAB+lenBC+lenAC); //возврат результата через формулу периметра треугольника
+        }
+        System.out.println ("Периметр треугольника "+ PerimetrABC);
+    }
+}
+
+
+
+
+
+}
